c3fd2e2a65a55a8743d82636bb9aaea3a6cd036f
Gabriel Regel <161236355+GabrielRegel@users.noreply.github.com>
2024-11-22 11:26:46 -0300

    Commit02

--- cezar.js
+++ cezar.js
@@ -25,4 +25,39 @@ function criptografar(frase, chave){
     }
     return resultado;
 }
-console.log(criptografar("bom dia, o joão é gay", "k"))
+
+function descriptografar(frase, chave){
+    const alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let resultado = "";
+
+    //Transformar a chave em um deslocamento
+    chave = chave.toUpperCase();
+    const deslocamento = alfabeto.indexOf(chave);
+
+    //Transformar toda a frase para letras minusculas 
+    frase = frase.toUpperCase();
+
+    //percorrer cada caractere da frase
+    for(let i = 0; i < frase.length; i++){
+        const char = frase[i];
+
+        //verificar se o caractere esta no alfabeto
+        if(alfabeto.includes(char)){
+            const index = alfabeto.indexOf(char);
+            const novoIndex = (index - deslocamento +26) % 26;
+            resultado += alfabeto[novoIndex];
+        }else{
+            //caracteres que nao estao no alfabeto permanecem inalterados
+            resultado += char;
+        }
+    }
+    return resultado;
+}
+
+
+console.log(criptografar("bom dia, o joão é gay", "k"));
+console.log(descriptografar("LYW NSK, Y TYÃY É QKI", "k"));
+
+
+
+
